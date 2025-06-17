263f68a512b6f770cfdb31eabc71ddf900c9716e
Umme Sadia Raisa <raisa.ummesadia@gmail.com>
2025-06-17 17:30:30 +0600

    guzzlehttp package added

--- config/params.php
+++ config/params.php
@@ -4,4 +4,14 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'keycloak' => [
+        'realm' => 'fortifymis',
+        'client_id' => 'fortify-cli',
+        'base_url' => 'https://dfqt.moind.gov.bd/iam/realms/fortifymis',
+        'token_url' => 'https://dfqt.moind.gov.bd/iam/realms/fortifymis/protocol/openid-connect/token',
+        'auth_url' => 'https://dfqt.moind.gov.bd/iam/realms/fortifymis/protocol/openid-connect/auth',
+        'userinfo_url' => 'https://dfqt.moind.gov.bd/iam/realms/fortifymis/protocol/openid-connect/userinfo',
+        'logout_url' => 'https://dfqt.moind.gov.bd/iam/realms/fortifymis/protocol/openid-connect/logout',
+    ],
+
 ];
